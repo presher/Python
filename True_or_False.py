b499c2a0126c16891a05ecaad41cceee4a57662c
Victor Jason Presher <presher@users.noreply.github.com>
2022-02-26 11:24:46 -0600

    Update True_or_False.py

--- True_or_False.py
+++ True_or_False.py
@@ -1,7 +1,7 @@
 language = "Python"
-print("1", language == "python")#False
+print("1", language == "python")
 
 age = 18
-print("2.", age >= 18)#True
-print("3.", age < 18)#False
-print("4.", age >= 18 and language == "Java")#False
+print("2.", age >= 18)
+print("3.", age < 18)
+print("4.", age >= 18 and language == "Java")
