13acfdfcb1b2b2b73cd802159c1cef1f8336e577
Victor Jason Presher <presher@users.noreply.github.com>
2022-02-28 12:10:33 -0600

    Create if-elif.py

---
+++ if-elif.py
@@ -0,0 +1,8 @@
+number = float(input("Input a number: "))
+if number >= 0:
+  if number == 0:
+      print("It is Zero!")
+  else:
+      print("Number is Positive number.")
+else:
+  print("Number is Negative number.")
